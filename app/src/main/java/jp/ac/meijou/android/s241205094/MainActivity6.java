778d3edaa49276bdd440cd475264b218cb28d246
Sunada Yuki <183799910+Sunachan1245@users.noreply.github.com>
2025-09-04 14:14:11 +0900

    MainActivity6、MainActivity7の追加
    - MainActivity6にOkHttpを使った画像取得処理を追加
    - activity_main6.xmlにImageView、EditText、Buttonを追加
    - MainActivity7を追加
    - activity_main7.xmlを追加
    - AndroidManifest.xmlにMainActivity6、MainActivity7を登録

---
+++ app/src/main/java/jp/ac/meijou/android/s241205094/MainActivity6.java
@@ -0,0 +1,61 @@
+package jp.ac.meijou.android.s241205094;
+
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import jp.ac.meijou.android.s241205094.databinding.ActivityMain6Binding;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity6 extends AppCompatActivity {
+    private ActivityMain6Binding binding;
+    private final OkHttpClient okHttpClient = new OkHttpClient();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMain6Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        getImage("https://placehold.jp/350x350.png");
+
+    }
+
+    private void getImage(String url){
+        //リクエスト先に画像URLを指定
+        var request = new Request.Builder()
+                .url(url)
+                .build();
+        //非同期通信でリクエスト
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                var bitmap = BitmapFactory.decodeStream(response.body().byteStream());
+                runOnUiThread(() -> binding.imageView.setImageBitmap(bitmap));
+            }
+        });
+    }
+}
