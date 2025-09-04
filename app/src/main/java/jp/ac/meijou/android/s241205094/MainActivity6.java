99a5d0544ff262b2cecbe438560849fcf8b28842
Sunada Yuki <183799910+Sunachan1245@users.noreply.github.com>
2025-09-04 14:54:36 +0900

    IPアドレスの取得と表示
    - ConnectivityManager を使用してネットワーク情報を取得
    - 取得したIPアドレスをTextViewに表示
    - activity_main6.xml のレイアウトを変更し、IPアドレス表示用のTextViewを追加

--- app/src/main/java/jp/ac/meijou/android/s241205094/MainActivity6.java
+++ app/src/main/java/jp/ac/meijou/android/s241205094/MainActivity6.java
@@ -1,6 +1,10 @@
 package jp.ac.meijou.android.s241205094;
 
 import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.LinkAddress;
+import android.net.Network;
+import android.net.NetworkCapabilities;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -12,6 +16,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 import java.io.IOException;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import jp.ac.meijou.android.s241205094.databinding.ActivityMain6Binding;
 import okhttp3.Call;
@@ -23,23 +28,28 @@ import okhttp3.Response;
 public class MainActivity6 extends AppCompatActivity {
     private ActivityMain6Binding binding;
     private final OkHttpClient okHttpClient = new OkHttpClient();
+    private ConnectivityManager connectivityManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         binding = ActivityMain6Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        connectivityManager = getSystemService(ConnectivityManager.class);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        getImage("https://placehold.jp/350x350.png");
+        //getImage("https://placehold.jp/350x350.png");
 
+        var currentNetwork = connectivityManager.getActiveNetwork();
+        updateTransport(currentNetwork);
+        updateIpAddress(currentNetwork);
     }
 
-    private void getImage(String url){
+    /*private void getImage(String url){
         //リクエスト先に画像URLを指定
         var request = new Request.Builder()
                 .url(url)
@@ -57,5 +67,31 @@ public class MainActivity6 extends AppCompatActivity {
                 runOnUiThread(() -> binding.imageView.setImageBitmap(bitmap));
             }
         });
+    }
+    */
+    private void updateTransport(Network network){
+        var caps = connectivityManager.getNetworkCapabilities(network);
+        if (caps != null) {
+            String transport;
+            if (caps.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                transport = "WIFI";
+            } else if (caps.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                transport = "蜂窝网络";
+            } else {
+                transport = "未知";
+            }
+            binding.textView7.setText(transport);
+        }
+    }
+
+    private void updateIpAddress(Network network){
+        var linkProperties = connectivityManager.getLinkProperties(network);
+        if (linkProperties != null){
+            var addresses = linkProperties.getLinkAddresses().stream()
+                    .map(LinkAddress::toString)
+                    .collect(Collectors.joining("\n"));
+            binding.textView9.setText(addresses);
+
+        }
     }
 }
